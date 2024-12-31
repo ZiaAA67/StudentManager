13f3b0ac3770bb1337f99ee9968a847887095a4f
MinkNhat <nquyenminhnhat0503@gmail.com>
2024-12-31 18:07:37 +0700

    format models

--- pythonStudentManager/StudentManager/models.py
+++ pythonStudentManager/StudentManager/models.py
@@ -8,6 +8,13 @@ from flask_login import UserMixin
 from enum import Enum as enum
 import hashlib
 
+from random import choice, randint
+from faker import Faker
+from datetime import datetime, timedelta
+from sqlalchemy.exc import IntegrityError
+
+fake = Faker()
+
 
 class Role(enum):
     STAFF = 1
@@ -156,191 +163,182 @@ class Notification(Base):
     content = Column(String(255), nullable=False)
 
 
+def generate_random_birth(min_age=15, max_age=20):
+    today = datetime.today()
+    start_date = today - timedelta(days=365 * max_age)
+    end_date = today - timedelta(days=365 * min_age)
+    return fake.date_between(start_date=start_date, end_date=end_date)
+
+
+def generate_unique_phone():
+    while True:
+        phone = ''.join([str(randint(0, 9)) for _ in range(10)])
+        if not UserInformation.query.filter_by(phone=phone).first():
+            return phone
+
+
+def generate_unique_email():
+    while True:
+        email = fake.unique.email()
+        if not UserInformation.query.filter_by(email=email).first():
+            return email
+
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        # new_rule = SchoolRules();
-        # db.session.add(new_rule)
-        # db.session.commit()
-        #
-        # admin_user_info = UserInformation(full_name="ADMIN USER",
-        #                                   gender=True,
-        #                                   address="hcm city",
-        #                                   birth=datetime(1999, 2, 12),
-        #                                   phone="023675348",
-        #                                   email="nguyen@ou.com",
-        #                                   role=Role.ADMIN)
-        # db.session.add(admin_user_info)
-        # db.session.commit()
-        # admin_detail = Administrator(id=admin_user_info.id)
-        # db.session.add(admin_detail)
-        # db.session.commit()
-        # username = "admin"
-        # password = str(hashlib.md5("123".encode('utf-8')).hexdigest())
-        # account = User(username=username,
-        #                password=password,
-        #                user_info_id=admin_user_info.id)
-        # db.session.add(account)
-        # db.session.commit()
-        #
-        # teacher_user_info = UserInformation(full_name="Teacher User",
-        #                                     gender=True,
-        #                                     address="hcm city",
-        #                                     birth=datetime(1999, 2, 12),
-        #                                     phone="023675344",
-        #                                     email="nguyenjss@ou.com",
-        #                                     role=Role.TEACHER)
-        # db.session.add(teacher_user_info)
-        # db.session.commit()
-        # teacher_detail = Teacher(id=teacher_user_info.id, degree="Khong co")
-        # db.session.add(teacher_detail)
-        # db.session.commit()
-        # username = "teacher"
-        # password = str(hashlib.md5("123".encode('utf-8')).hexdigest())
-        # account = User(username=username,
-        #                password=password,
-        #                user_info_id=teacher_user_info.id)
-        # db.session.add(account)
-        # db.session.commit()
-        #
-        # employee_user_info = UserInformation(full_name="Employee User",
-        #                                      gender=True,
-        #                                      address="hcm city",
-        #                                      birth=datetime(1999, 2, 12),
-        #                                      phone="023675343",
-        #                                      email="nguyenjsa@ou.com",
-        #                                      role=Role.STAFF)
-        # db.session.add(employee_user_info)
-        # db.session.commit()
-        # username = "employee"
-        # password = str(hashlib.md5("123".encode('utf-8')).hexdigest())
-        # account = User(username=username,
-        #                password=password,
-        #                user_info_id=employee_user_info.id)
-        # db.session.add(account)
-        # db.session.commit()
-        #
-        # student_user_info = UserInformation(full_name="Student User",
-        #                                     gender=True,
-        #                                     address="hcm city",
-        #                                     birth=datetime(1999, 2, 12),
-        #                                     phone="023695343",
-        #                                     email="nguynjsa@ou.com",
-        #                                     role=Role.STUDENT)
-        # db.session.add(student_user_info)
-        # db.session.commit()
-        # student_detail = Student(id=student_user_info.id, grade=Grade.GRADE_10)
-        # db.session.add(student_detail)
-        # db.session.commit()
-#
-# # Sample
-# from random import choice, randint
-# from faker import Faker
-# from datetime import datetime, timedelta
-# from sqlalchemy.exc import IntegrityError
-#
-# fake = Faker()
-#
-#
-# def generate_random_birth(min_age=15, max_age=20):
-#     today = datetime.today()
-#     start_date = today - timedelta(days=365 * max_age)
-#     end_date = today - timedelta(days=365 * min_age)
-#     return fake.date_between(start_date=start_date, end_date=end_date)
-#
-#
-# def generate_unique_phone():
-#     while True:
-#         phone = ''.join([str(randint(0, 9)) for _ in range(10)])
-#         if not UserInformation.query.filter_by(phone=phone).first():
-#             return phone
-#
-#
-# def generate_unique_email():
-#     while True:
-#         email = fake.unique.email()
-#         if not UserInformation.query.filter_by(email=email).first():
-#             return email
-#
-#
-# if __name__ == "__main__":
-#     with app.app_context():
-#         # Create teachers
-#         teachers = []
-#         for _ in range(6):
-#             full_name = fake.name()
-#             gender = choice([True, False])
-#             address = fake.address()
-#             birth = generate_random_birth(min_age=25, max_age=60)
-#             phone = generate_unique_phone()
-#             email = generate_unique_email()
-#             role = Role.TEACHER
-#             degree = choice(["Cử nhân", "Thạc sĩ", "Tiến sĩ"])
-#
-#             teacher_info = UserInformation(
-#                 full_name=full_name,
-#                 gender=gender,
-#                 address=address,
-#                 birth=birth,
-#                 phone=phone,
-#                 email=email,
-#                 role=role
-#             )
-#             db.session.add(teacher_info)
-#             db.session.commit()
-#
-#             teacher_detail = Teacher(id=teacher_info.id, degree=degree)
-#             db.session.add(teacher_detail)
-#             db.session.commit()
-#
-#             teachers.append(teacher_detail)
-#
-#             print(f"Successfully added {len(teachers)} teachers.")
-#
-#         # Create classes
-#         classes = []
-#         class_names = ["10A", "10B", "11A", "11B", "12A", "12B"]
-#         for class_name in class_names:
-#             grade_value = int(class_name[:2])
-#             grade = Grade(grade_value)
-#             new_class = Class(name=class_name, grade=grade)
-#             db.session.add(new_class)
-#             db.session.commit()
-#             classes.append(new_class)
-#
-#         print(f"Successfully added {len(classes)} classes.")
-#
-#         # Create students
-#         students = []
-#         student_index = 0
-#         for class_obj in classes:
-#             for _ in range(40):
-#                 if student_index >= 30:
-#                     break
-#
-#                 full_name = fake.name()
-#                 gender = choice([True, False])
-#                 address = fake.address()
-#                 birth = generate_random_birth()
-#                 phone = generate_unique_phone()
-#                 email = generate_unique_email()
-#                 role = Role.STUDENT
-#
-#                 student_info = UserInformation(
-#                     full_name=full_name,
-#                     gender=gender,
-#                     address=address,
-#                     birth=birth,
-#                     phone=phone,
-#                     email=email,
-#                     role=role
-#                 )
-#                 db.session.add(student_info)
-#                 db.session.commit()
-#
-#                 student_detail = Student(id=student_info.id, grade=class_obj.grade, class_id=class_obj.id)
-#                 db.session.add(student_detail)
-#                 db.session.commit()
-#
-#                 students.append(student_detail)
-#                 student_index += 1
+        new_rule = SchoolRules();
+        db.session.add(new_rule)
+        db.session.commit()
+
+        admin_user_info = UserInformation(full_name="ADMIN USER",
+                                          gender=True,
+                                          address="hcm city",
+                                          birth=datetime(1999, 2, 12),
+                                          phone="023675348",
+                                          email="nguyen@ou.com",
+                                          role=Role.ADMIN)
+        db.session.add(admin_user_info)
+        db.session.commit()
+        admin_detail = Administrator(id=admin_user_info.id)
+        db.session.add(admin_detail)
+        db.session.commit()
+        username = "admin"
+        password = str(hashlib.md5("123".encode('utf-8')).hexdigest())
+        account = User(username=username,
+                       password=password,
+                       user_info_id=admin_user_info.id)
+        db.session.add(account)
+        db.session.commit()
+
+        teacher_user_info = UserInformation(full_name="Teacher User",
+                                            gender=True,
+                                            address="hcm city",
+                                            birth=datetime(1999, 2, 12),
+                                            phone="023675344",
+                                            email="nguyenjss@ou.com",
+                                            role=Role.TEACHER)
+        db.session.add(teacher_user_info)
+        db.session.commit()
+        teacher_detail = Teacher(id=teacher_user_info.id, degree="Khong co")
+        db.session.add(teacher_detail)
+        db.session.commit()
+        username = "teacher"
+        password = str(hashlib.md5("123".encode('utf-8')).hexdigest())
+        account = User(username=username,
+                       password=password,
+                       user_info_id=teacher_user_info.id)
+        db.session.add(account)
+        db.session.commit()
+
+        employee_user_info = UserInformation(full_name="Employee User",
+                                             gender=True,
+                                             address="hcm city",
+                                             birth=datetime(1999, 2, 12),
+                                             phone="023675343",
+                                             email="nguyenjsa@ou.com",
+                                             role=Role.STAFF)
+        db.session.add(employee_user_info)
+        db.session.commit()
+        username = "employee"
+        password = str(hashlib.md5("123".encode('utf-8')).hexdigest())
+        account = User(username=username,
+                       password=password,
+                       user_info_id=employee_user_info.id)
+        db.session.add(account)
+        db.session.commit()
+
+        student_user_info = UserInformation(full_name="Student User",
+                                            gender=True,
+                                            address="hcm city",
+                                            birth=datetime(1999, 2, 12),
+                                            phone="023695343",
+                                            email="nguynjsa@ou.com",
+                                            role=Role.STUDENT)
+        db.session.add(student_user_info)
+        db.session.commit()
+        student_detail = Student(id=student_user_info.id, grade=Grade.GRADE_10)
+        db.session.add(student_detail)
+        db.session.commit()
+
+        # Create teachers
+        teachers = []
+        for _ in range(6):
+            full_name = fake.name()
+            gender = choice([True, False])
+            address = fake.address()
+            birth = generate_random_birth(min_age=25, max_age=60)
+            phone = generate_unique_phone()
+            email = generate_unique_email()
+            role = Role.TEACHER
+            degree = choice(["Cử nhân", "Thạc sĩ", "Tiến sĩ"])
+
+            teacher_info = UserInformation(
+                full_name=full_name,
+                gender=gender,
+                address=address,
+                birth=birth,
+                phone=phone,
+                email=email,
+                role=role
+            )
+            db.session.add(teacher_info)
+            db.session.commit()
+
+            teacher_detail = Teacher(id=teacher_info.id, degree=degree)
+            db.session.add(teacher_detail)
+            db.session.commit()
+
+            teachers.append(teacher_detail)
+
+            print(f"Successfully added {len(teachers)} teachers.")
+
+        # Create classes
+        classes = []
+        class_names = ["10A", "10B", "11A", "11B", "12A", "12B"]
+        for class_name in class_names:
+            grade_value = int(class_name[:2])
+            grade = Grade(grade_value)
+            new_class = Class(name=class_name, grade=grade)
+            db.session.add(new_class)
+            db.session.commit()
+            classes.append(new_class)
+
+        print(f"Successfully added {len(classes)} classes.")
+
+        # Create students
+        students = []
+        student_index = 0
+        for class_obj in classes:
+            for _ in range(40):
+                if student_index >= 30:
+                    break
+
+                full_name = fake.name()
+                gender = choice([True, False])
+                address = fake.address()
+                birth = generate_random_birth()
+                phone = generate_unique_phone()
+                email = generate_unique_email()
+                role = Role.STUDENT
+
+                student_info = UserInformation(
+                    full_name=full_name,
+                    gender=gender,
+                    address=address,
+                    birth=birth,
+                    phone=phone,
+                    email=email,
+                    role=role
+                )
+                db.session.add(student_info)
+                db.session.commit()
+
+                student_detail = Student(id=student_info.id, grade=class_obj.grade, class_id=class_obj.id)
+                db.session.add(student_detail)
+                db.session.commit()
+
+                students.append(student_detail)
+                student_index += 1
+
+
